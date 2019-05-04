971004395f18ea0751b4de6c96f81e78b6811ef4
jakub <jstarzyk98@gmail.com>
2019-05-04 18:56:35 +0200

    Modify 'money' interface

--- Starzyk_Jakub_4/thrift-bank/src/main/thrift/money.thrift
+++ Starzyk_Jakub_4/thrift-bank/src/main/thrift/money.thrift
@@ -1,28 +1,30 @@
 namespace java money
 
-typedef i32 FractionAmount
-typedef i64 BaseAmount
+//typedef i32 FractionAmount
+//typedef i64 BaseAmount
 
 enum Currency {
     PLN,
     USD,
     EUR,
     GBP,
-    CHF
+    CHF,
 }
 
-const map<Currency, FractionAmount> SUBUNITS = {
-    PLN: 100,
-    USD: 100,
-    EUR: 100,
-    GBP: 100,
-    CHF: 100,
-}
+//const map<Currency, FractionAmount> SUBUNITS = {
+//    PLN: 100,
+//    USD: 100,
+//    EUR: 100,
+//    GBP: 100,
+//    CHF: 100,
+//}
+//const i32 SUBUNITS = 100
 
 struct Money {
     1: Currency currency,
-    2: BaseAmount baseAmount,
-    3: FractionAmount fractionAmount,
+    2: double amount
+//    2: BaseAmount baseAmount,
+//    3: FractionAmount fractionAmount,
 }
 
 exception InvalidAmount {
